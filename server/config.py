485016d7e999c47c04e9b2b15b2030b91318a505
Njagi Githaiga <njagigithaiga@gmail.com>
2024-08-18 01:31:49 +0300

    Changes project struture

--- server/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    FIREBASE_CREDENTIALS = os.getenv('FIREBASE_CREDENTIALS', 'path_to_firebase_admin_sdk.json')
