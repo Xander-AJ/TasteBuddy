c7e12a44db51441a64b2bf04ddb5c808113ada66
Xander-AJ <xander.kamau7@gmail.com>
2024-08-20 04:53:28 +0300

    Refined API functions to handle new backend endpoints.

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,3 @@
-// src/api.js
 import axios from 'axios';
 
 const API_URL = 'http://localhost:5000'; // Adjust this to your backend URL
@@ -10,12 +9,22 @@ const api = axios.create({
   },
 });
 
+// Interceptor to attach the correct token based on the request URL
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  // Determine if the request is for an admin route
+  const isAdminRoute = config.url.includes('/admin');
+
+  // Retrieve the appropriate token from localStorage
+  const token = isAdminRoute ? localStorage.getItem('adminToken') : localStorage.getItem('token');
+
+  // If a token exists, attach it to the Authorization header
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
+
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
